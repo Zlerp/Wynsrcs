2dd8425e2e943a85ef79a4309f74a708f7f17e4a
Volney Tejeda <volneytejeda10@gmail.com>
2015-09-06 15:52:21 -0400

    add validations to sources

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,3 +1,5 @@
 class Source < ActiveRecord::Base
   belongs_to :user
+
+  validates_presence_of :link, :category, :description, :tags 
 end
--- spec/models/source_spec.rb
+++ spec/models/source_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Source, type: :model do
- it {should belong_to(:user) }
+  it {should belong_to(:user) }
+
+  it { should validate_presence_of(:link) }
+  it { should validate_presence_of(:category) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:tags) }
+
 end
