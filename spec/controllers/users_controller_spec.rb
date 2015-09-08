e6482b89890efffdfcb7dbf78b9505fd3c1afe4a
Volney Tejeda <volneytejeda10@gmail.com>
2015-09-08 17:40:11 -0400

    Add test for user index

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:valid_attributes) { { username: "WynUser",
+                             email: "wynuser@example.com",
+                             password: "password",
+                             password_confirmation: "password" } }
+  describe "GET index" do
+    it "assigns @users" do
+      user = User.create! valid_attributes
+      get :index
+      expect(assigns(:users)).to eq([user])
+    end
+  end
 
+  it "renders the :index view" do
+    get :index
+    response.should render_template :index
+  end
 end
