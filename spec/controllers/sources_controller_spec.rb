0341c2902b7b1025ee7dc46a94162bcbcacf8ff3
Volney Tejeda <volneytejeda10@gmail.com>
2015-09-07 23:00:44 -0400

    Add test for source index action

--- spec/controllers/sources_controller_spec.rb
+++ spec/controllers/sources_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe SourcesController, type: :controller do
+  let(:valid_attributes) { { link: "Thislink.com",
+                             category: "Ruby",
+                             description: "blah",
+                             tags: "ruby rails" } }
+  describe "GET index" do
+    it "assigns @sources" do
+      source = Source.create! valid_attributes
+      get :index
+      expect(assigns(:sources)).to eq([source])
+    end
+  end
 
+  it "renders the :index view" do
+    get :index
+    response.should render_template :index
+  end
 end
